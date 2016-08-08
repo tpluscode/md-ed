fd99c509cea6a564ad322709bb1c983131a81e69
tpluscode <tpluscode@users.noreply.github.com>
2016-08-08 22:45:03 +0200

    install dependencies

--- config.js
+++ config.js
@@ -14,7 +14,11 @@ System.config({
   },
 
   map: {
+    "Hypercubed/systemjs-plugin-html": "github:Hypercubed/systemjs-plugin-html@0.0.8",
     "typescript": "npm:typescript@1.8.10",
+    "github:Hypercubed/systemjs-plugin-html@0.0.8": {
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.7.22"
+    },
     "github:jspm/nodelibs-os@0.1.0": {
       "os-browserify": "npm:os-browserify@0.1.2"
     },
