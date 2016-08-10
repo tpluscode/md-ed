33dc9b1fff7e728bcbffc409e6a81802d51cf994
tpluscode <tpluscode@users.noreply.github.com>
2016-08-10 20:15:14 +0200

    try a different main for jspm

---
+++ md-ed.js
@@ -0,0 +1,5 @@
+System.import("bower_components/paper-input/paper-textarea.html!");
+System.import("bower_components/paper-tabs/paper-tabs.html!");
+System.import("bower_components/iron-pages/iron-pages.html!");
+System.import("tpluscode/md-ed/dist/build.html!");
+System.import("tpluscode/md-ed/dist/build");
