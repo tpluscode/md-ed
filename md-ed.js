bbaa4ee67688260aad0eef05b2472fa0bb2cb005
tpluscode <tpluscode@users.noreply.github.com>
2016-08-11 20:14:12 +0200

    don't need this for entrypoint

--- md-ed.js
+++
@@ -1,5 +0,0 @@
-import "bower_components/paper-input/paper-textarea.html!";
-import "bower_components/paper-tabs/paper-tabs.html!";
-import "bower_components/iron-pages/iron-pages.html!";
-import "./dist/build.html!";
-import "./dist/build";
