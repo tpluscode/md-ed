5b019b9925303d37f249d68b3366c1cf38be40c7
tpluscode <tpluscode@users.noreply.github.com>
2016-08-13 21:36:47 +0200

    simplify jspm bundling

--- md-ed.js
+++ md-ed.js
@@ -1,5 +1,8 @@
-import './src/bower-deps'
-import './dist/jspm/bundle.html!'
+import "bower_components/paper-input/paper-textarea.html!Hypercubed/systemjs-plugin-html";
+import "bower_components/paper-tabs/paper-tabs.html!Hypercubed/systemjs-plugin-html";
+import "bower_components/iron-pages/iron-pages.html!Hypercubed/systemjs-plugin-html";
+
+import './dist/jspm/bundle.html!Hypercubed/systemjs-plugin-html'
 import './dist/jspm/bundle'
 
 System.import('src/md-ed.ts');
--- src/bower-deps.js
+++
@@ -1,3 +0,0 @@
-import "bower_components/paper-input/paper-textarea.html!";
-import "bower_components/paper-tabs/paper-tabs.html!";
-import "bower_components/iron-pages/iron-pages.html!";
