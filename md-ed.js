0445ee9e25a3fdb8ac32f083605b53aa4d6aa2ba
tpluscode <tpluscode@users.noreply.github.com>
2016-08-13 21:49:18 +0200

    add polymer-ts as bower import

--- md-ed.js
+++ md-ed.js
@@ -1,8 +1,9 @@
-import "bower_components/paper-input/paper-textarea.html!Hypercubed/systemjs-plugin-html";
-import "bower_components/paper-tabs/paper-tabs.html!Hypercubed/systemjs-plugin-html";
-import "bower_components/iron-pages/iron-pages.html!Hypercubed/systemjs-plugin-html";
+import "bower_components/polymer-ts/polymer-ts.min.html!";
+import "bower_components/paper-input/paper-textarea.html!";
+import "bower_components/paper-tabs/paper-tabs.html!";
+import "bower_components/iron-pages/iron-pages.html!";
 
-import './dist/jspm/bundle.html!Hypercubed/systemjs-plugin-html'
+import './dist/jspm/bundle.html!'
 import './dist/jspm/bundle'
 
 System.import('src/md-ed.ts');
